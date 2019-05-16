b58ba67023fac32f4c5c261034b5c16bce9bd86a
eccard <felipelugao@id.uff.br>
2019-05-15 22:08:51 -0300

    Refactor class

--- app/src/main/java/com/r5k/contacerveja/ui/main/interactor/DefaulDrinksForBill.kt
+++
@@ -1,3 +0,0 @@
-package com.r5k.contacerveja.ui.main.interactor
-
-data class DefaulDrinksForBill(val p1:Boolean, val p2:Boolean, val p3:Boolean)
---
+++ app/src/main/java/com/r5k/contacerveja/ui/main/interactor/DefaultDrinksForBill.kt
@@ -0,0 +1,6 @@
+package com.r5k.contacerveja.ui.main.interactor
+
+data class DefaultDrinksForBill(val billId: Long,
+                                val drink1Id:Long,
+                                val drink2Id:Long,
+                                val drink3Id:Long)
