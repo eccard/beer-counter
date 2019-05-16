8ff4480b4db360a2d6a29fb4bfe919d616c56019
eccard <felipelugao@id.uff.br>
2019-05-15 23:36:52 -0300

    removes fragments

--- app/src/main/java/com/r5k/contacerveja/ui/MyPagerAdapter.kt
+++ app/src/main/java/com/r5k/contacerveja/ui/MyPagerAdapter.kt
@@ -11,12 +11,7 @@ class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
             0 -> {
                 FirstFragment()
             }
-            1 -> {
-                SecondFragment()
-            }
-            2 -> {
-                ThirdFragment()
-            }
+
             else -> {
                 return PlusFragment()
             }
