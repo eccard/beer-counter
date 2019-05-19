7b1d92d90b866165a38cf570deb61650129e6632
eccard <felipelugao@id.uff.br>
2019-05-19 00:03:33 -0300

    Display qnt of drinks

--- app/src/main/java/com/r5k/contacerveja/ui/drink/view/DrinkFragment.kt
+++ app/src/main/java/com/r5k/contacerveja/ui/drink/view/DrinkFragment.kt
@@ -5,22 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.r5k.contacerveja.R
+import com.r5k.contacerveja.data.database.repository.drink.Drink
 import com.r5k.contacerveja.ui.base.BaseFragment
 import com.r5k.contacerveja.ui.drink.interactor.DrinkMVPInteractor
 import com.r5k.contacerveja.ui.drink.presenter.DrinkMVPPresenter
+import kotlinx.android.synthetic.main.fragment_generic_drink.*
 import javax.inject.Inject
 
 
 class DrinkFragment : BaseFragment(),DrinkMVPView {
 
     companion object {
-        fun newInstance(): DrinkFragment{
-            return DrinkFragment()
+        private const val MY_DRINK = "drink"
+
+        fun newInstance(drink:Drink): DrinkFragment = DrinkFragment().apply {
+            arguments = Bundle().apply{
+                putParcelable(MY_DRINK,drink)
+            }
+
         }
     }
 
     @Inject
     internal lateinit var presenter : DrinkMVPPresenter<DrinkMVPView,DrinkMVPInteractor>
+
+    private var mDrink: Drink? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mDrink = arguments?.getParcelable(MY_DRINK)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_generic_drink,container,false)
@@ -32,6 +46,7 @@ class DrinkFragment : BaseFragment(),DrinkMVPView {
     }
 
     override fun setUp() {
+        mDrink!!.qnt?.let { qnt_drink.text = it.toString() }
     }
 
 
