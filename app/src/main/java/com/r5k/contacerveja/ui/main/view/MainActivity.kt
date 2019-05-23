27ce6298eb32680da8592996668824752135b29e
eccard <felipelugao@id.uff.br>
2019-05-22 23:52:15 -0300

    Add menu and dialog to add new drink

--- app/src/main/java/com/r5k/contacerveja/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/r5k/contacerveja/ui/main/view/MainActivity.kt
@@ -2,7 +2,11 @@ package com.r5k.contacerveja.ui.main.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputEditText
 import com.r5k.contacerveja.R
 import com.r5k.contacerveja.data.database.repository.drink.Drink
 import com.r5k.contacerveja.ui.drink.DrinkPagerAdapter
@@ -68,4 +72,57 @@ class MainActivity : BaseActivity(), MainMVPView, HasSupportFragmentInjector {
         Log.d(TAG, "loadDefaultDrinks size = $drinks")
         fragmentAdapter.setDrinkList(drinks)
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.action_add_drink -> {
+                showAddDrinkDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun showAddDrinkDialog() {
+        val context = this
+        val builder = AlertDialog.Builder(context)
+
+        // https://stackoverflow.com/questions/10695103/creating-custom-alertdialog-what-is-the-root-view
+        // Seems ok to inflate view with null rootView
+        val view = layoutInflater.inflate(R.layout.dialog_new_drink, null)
+
+        val newDrinkEditText = view.findViewById(R.id.edt_new_drink_name) as TextInputEditText
+
+        builder.setView(view)
+
+        // set up the ok button
+        builder.setPositiveButton(android.R.string.ok) { dialog, p1 ->
+            val newDrinkName = newDrinkEditText.text
+            var isValid = true
+            if (newDrinkName!!.isBlank()) {
+                newDrinkEditText.error = getString(R.string.validation_empty)
+                isValid = false
+            }
+
+            if (isValid) {
+                Log.d(TAG, "Adding newDrinkName $newDrinkName")
+                presenter.addNewDrink(newDrinkName.toString())
+            }
+
+            if (isValid) {
+                dialog.dismiss()
+            }
+        }
+
+        builder.setNegativeButton(android.R.string.cancel) { dialog, p1 ->
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
+
 }
