f016ec75f37002904be002b32c7208faaa3482ed
eccard <felipelugao@id.uff.br>
2019-10-17 06:54:42 -0300

    Update app/src/main/java/com/r5k/contacerveja/util/AppConstants.kt
    Co-Authored-By: lssalgado <lucas.s_salgado@hotmail.com>

--- app/src/main/java/com/r5k/contacerveja/util/AppConstants.kt
+++ app/src/main/java/com/r5k/contacerveja/util/AppConstants.kt
@@ -3,6 +3,6 @@ package com.r5k.contacerveja.util
 object AppConstants{
     internal val APP_DB_NAME = "beer_count.db"
     internal val ACTION_NEW_BILL = "ACTION_ADD_NEW_BILL"
-    internal val ACTION_LOAD_BILL = "ACTION_ADD_LOAD_BILL"
+    internal val ACTION_LOAD_BILL = "ACTION_LOAD_BILL"
     internal val KEY_BILL_ID = "KEY_BILL_ID"
 }
