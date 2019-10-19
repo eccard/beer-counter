320de7b6e97fe9ada19ab37be61b4e9113aeb822
Matheus Lima <matheuslima9412@gmail.com>
2019-10-18 22:45:26 -0300

    Adds factory to provide viewmodel instance

---
+++ app/src/main/java/com/r5k/contacerveja/di/factory/ViewModelFactoryModule.kt
@@ -0,0 +1,32 @@
+package com.r5k.contacerveja.di.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.r5k.contacerveja.ui.drink.view.DrinkViewModel
+import com.r5k.contacerveja.ui.main.view.MainViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import javax.inject.Singleton
+
+@Suppress("unused")
+@Module
+abstract class ViewModelFactoryModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DrinkViewModel::class)
+    abstract fun bindDrinkViewModel(drinkViewModel: DrinkViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    @Singleton
+    abstract fun bindMainViewModel(mainViewModel: MainViewModel): ViewModel
+
+
+
+    @Binds
+    @Singleton
+    abstract fun bindViewModelFactory(viewModelFactory: ViewModelProviderFactory): ViewModelProvider.Factory
+}
