8a67b88c93a8160dc6df2e216f150c4c404396c1
Matheus Lima <matheuslima9412@gmail.com>
2019-10-18 22:50:20 -0300

    Updates to provide interactors

--- app/src/main/java/com/r5k/contacerveja/di/module/AppModule.kt
+++ app/src/main/java/com/r5k/contacerveja/di/module/AppModule.kt
@@ -8,12 +8,15 @@ import com.r5k.contacerveja.data.database.repository.bill.BillsRepo
 import com.r5k.contacerveja.data.database.repository.bill.BillsRepository
 import com.r5k.contacerveja.data.database.repository.drink.DrinksRepo
 import com.r5k.contacerveja.data.database.repository.drink.DrinksRepository
+import com.r5k.contacerveja.di.factory.ViewModelFactoryModule
+import com.r5k.contacerveja.ui.drink.interactor.DrinkInteractor
+import com.r5k.contacerveja.ui.main.interactor.MainInteractor
 import com.r5k.contacerveja.util.AppConstants
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Module
+@Module(includes = [ViewModelFactoryModule::class])
 class AppModule {
 
     @Provides
@@ -41,6 +44,17 @@ class AppModule {
     @Provides
     @Singleton
     internal fun provideBillsRepoHelper(appDatabase: AppDatabase): BillsRepo = BillsRepository(appDatabase.billsDao())
+
+
+
+    @Provides
+    @Singleton
+    internal fun provideMainInteractor(drinksRepository: DrinksRepository,billsRepository: BillsRepository) =
+        MainInteractor(drinksRepository,billsRepository)
+
+    @Provides
+    @Singleton
+    internal fun provideDrinkInteractor(drinksRepository: DrinksRepository) = DrinkInteractor(drinksRepository)
 
 
 }
