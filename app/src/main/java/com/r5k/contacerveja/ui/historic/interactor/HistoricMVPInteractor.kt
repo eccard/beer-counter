c02f9f0fe90e0a286e6be85793cb386b2de73b16
Matheus Lima <matheuslima9412@gmail.com>
2019-10-20 10:09:41 -0300

    Removes mvp interfaces

--- app/src/main/java/com/r5k/contacerveja/ui/historic/interactor/HistoricMVPInteractor.kt
+++
@@ -1,9 +0,0 @@
-package com.r5k.contacerveja.ui.historic.interactor
-
-import com.r5k.contacerveja.data.database.repository.bill.Bill
-import com.r5k.contacerveja.ui.base.MVPInteractor
-import kotlinx.coroutines.Deferred
-
-interface HistoricMVPInteractor : MVPInteractor {
-    suspend fun loadBills() : Deferred<List<Bill>>
-}
